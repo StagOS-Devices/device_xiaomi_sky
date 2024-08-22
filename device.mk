0bcff9a2a9826dad166b1a42b8dbe78d8e5018c0
minaripenguin <dhanushvozh345@gmail.com>
2024-08-22 06:11:55 -0700

    Revert "oneplus dolby added"
    This reverts commit 5a4ac30ec57ee58ff4970dd01d8a6d5337b4a15a.

--- device.mk
+++ device.mk
@@ -179,10 +179,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
